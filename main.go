760241d965a47f8382bb81409226920a9e3ae41b
Niko Abeler <niko@rerere.org>
2022-07-19 21:25:58 +0200

    create posts

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"path"
+	"time"
+	"fmt"
 )
 
 func CreateNewUser(repo string, name string) {
@@ -13,8 +15,35 @@ func CreateNewUser(repo string, name string) {
 	user_dir := path.Join(repo, name)
 	os.Mkdir(user_dir, 0755)
 	os.Mkdir(path.Join(user_dir, "meta"), 0755)
+	// create public folder
+	os.Mkdir(path.Join(user_dir, "public"), 0755)
+
+	// create Meta files
 	os.WriteFile(path.Join(user_dir, "meta", "VERSION"), []byte("0.0.1"), 0644)
 	os.WriteFile(path.Join(user_dir, "meta", "base.html"), []byte("<html><body><{{content}}/body></html>"), 0644)
+}
+
+func CreateNewPost(repo string, user string, title string) {
+	timestamp := time.Now().UTC().Unix()
+	folder_name := fmt.Sprintf("%d-%s", timestamp, title)
+	post_dir := path.Join(repo, user, "public", folder_name)
+
+	// if post already exists, add -n to the end of the name
+	i := 0
+	for {
+		if _, err := os.Stat(post_dir); err == nil {
+			i++
+			folder_name = fmt.Sprintf("%d-%s-%d", timestamp, title, i)
+			post_dir = path.Join(repo, user, "public", folder_name)
+		} else {
+			break
+		}
+	}
+
+	initial_content := "# " + title
+	// create post file
+	os.Mkdir(post_dir, 0755)
+	os.WriteFile(path.Join(post_dir, "index.md"), []byte(initial_content), 0644)
 }
 
 func main() {
