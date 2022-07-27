09a084e5faa635e09228502c70a28e498ab70a42
Niko Abeler <niko@rerere.org>
2022-07-27 21:26:37 +0200

    a bit more style

--- post.go
+++ post.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/yuin/goldmark"
 	meta "github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
 )
 
@@ -43,6 +44,7 @@ func (post Post) MarkdownData() (bytes.Buffer, map[string]interface{}) {
 	markdown := goldmark.New(
 		goldmark.WithExtensions(
 			meta.Meta,
+			extension.GFM,
 		),
 	)
 	var buf bytes.Buffer
