a08740dec43b789b81714ba5a29a7302f34910a6
Niko Abeler <niko@rerere.org>
2022-08-03 19:43:00 +0200

    clean up handlers

--- cmd/owl-web/handler.go
+++ cmd/owl-web/handler.go
@@ -3,11 +3,13 @@ package main
 import (
 	"h4kor/owl-blogs"
 	"net/http"
+	"os"
+	"path"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func RepoIndexHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+func repoIndexHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		html, err := owl.RenderUserList(repo)
 
@@ -19,5 +21,89 @@ func RepoIndexHandler(repo owl.Repository) func(http.ResponseWriter, *http.Reque
 		}
 		println("Rendering index")
 		w.Write([]byte(html))
+	}
+}
+
+func userIndexHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		userName := ps.ByName("user")
+		user, err := repo.GetUser(userName)
+		if err != nil {
+			println("Error getting user: ", err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("User not found"))
+			return
+		}
+		html, err := owl.RenderIndexPage(user)
+		if err != nil {
+			println("Error rendering index page: ", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Internal server error"))
+			return
+		}
+		println("Rendering index page for user", userName)
+		w.Write([]byte(html))
+	}
+}
+
+func postHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		userName := ps.ByName("user")
+		postId := ps.ByName("post")
+		user, err := repo.GetUser(userName)
+		if err != nil {
+			println("Error getting user: ", err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("User not found"))
+			return
+		}
+		post, err := user.GetPost(postId)
+		if err != nil {
+			println("Error getting post: ", err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Post not found"))
+			return
+		}
+		html, err := owl.RenderPost(post)
+		if err != nil {
+			println("Error rendering post: ", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Internal server error"))
+			return
+		}
+		println("Rendering post", postId)
+		w.Write([]byte(html))
+
+	}
+}
+
+func postMediaHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		userName := ps.ByName("user")
+		postId := ps.ByName("post")
+		filepath := ps.ByName("filepath")
+		user, err := repo.GetUser(userName)
+
+		if err != nil {
+			println("Error getting user: ", err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("User not found"))
+			return
+		}
+		post, err := user.GetPost(postId)
+		if err != nil {
+			println("Error getting post: ", err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Post not found"))
+			return
+		}
+		filepath = path.Join(post.MediaDir(), filepath)
+		if _, err := os.Stat(filepath); err != nil {
+			println("Error getting file: ", err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("File not found"))
+			return
+		}
+		http.ServeFile(w, r, filepath)
 	}
 }
--- cmd/owl-web/main.go
+++ cmd/owl-web/main.go
@@ -4,101 +4,16 @@ import (
 	"h4kor/owl-blogs"
 	"net/http"
 	"os"
-	"path"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func userHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		userName := ps.ByName("user")
-		user, err := repo.GetUser(userName)
-		if err != nil {
-			println("Error getting user: ", err.Error())
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("User not found"))
-			return
-		}
-		html, err := owl.RenderIndexPage(user)
-		if err != nil {
-			println("Error rendering index page: ", err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal server error"))
-			return
-		}
-		println("Rendering index page for user", userName)
-		w.Write([]byte(html))
-	}
-}
-
-func postHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		userName := ps.ByName("user")
-		postId := ps.ByName("post")
-		user, err := repo.GetUser(userName)
-		if err != nil {
-			println("Error getting user: ", err.Error())
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("User not found"))
-			return
-		}
-		post, err := user.GetPost(postId)
-		if err != nil {
-			println("Error getting post: ", err.Error())
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("Post not found"))
-			return
-		}
-		html, err := owl.RenderPost(post)
-		if err != nil {
-			println("Error rendering post: ", err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal server error"))
-			return
-		}
-		println("Rendering post", postId)
-		w.Write([]byte(html))
-
-	}
-}
-
-func postMediaHandler(repo owl.Repository) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		userName := ps.ByName("user")
-		postId := ps.ByName("post")
-		filepath := ps.ByName("filepath")
-		user, err := repo.GetUser(userName)
-
-		if err != nil {
-			println("Error getting user: ", err.Error())
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("User not found"))
-			return
-		}
-		post, err := user.GetPost(postId)
-		if err != nil {
-			println("Error getting post: ", err.Error())
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("Post not found"))
-			return
-		}
-		filepath = path.Join(post.MediaDir(), filepath)
-		if _, err := os.Stat(filepath); err != nil {
-			println("Error getting file: ", err.Error())
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("File not found"))
-			return
-		}
-		http.ServeFile(w, r, filepath)
-	}
-}
-
 func Router(repo owl.Repository) http.Handler {
 	router := httprouter.New()
 	router.ServeFiles("/static/*filepath", http.Dir(repo.StaticDir()))
-	router.GET("/", RepoIndexHandler(repo))
-	router.GET("/user/:user/", userHandler(repo))
+	router.GET("/", repoIndexHandler(repo))
+	router.GET("/user/:user/", userIndexHandler(repo))
 	router.GET("/user/:user/posts/:post/", postHandler(repo))
 	router.GET("/user/:user/posts/:post/media/*filepath", postMediaHandler(repo))
 	return router
