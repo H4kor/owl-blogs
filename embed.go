4540797ccee983ae1583ad4faf12cdb56cc7fe0d
Niko Abeler <niko@rerere.org>
2023-07-19 20:59:43 +0200

    cleanup

--- embed.go
+++
@@ -1,6 +0,0 @@
-package owl
-
-import "embed"
-
-//go:embed embed/*
-var embed_files embed.FS
--- release.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t git.libove.org/h4kor/owl-blogs:$1
-docker push git.libove.org/h4kor/owl-blogs:$1
