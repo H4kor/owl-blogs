34e4984af9a778af776a988e35da4b1fb379672f
Niko Abeler <niko@rerere.org>
2023-07-25 21:39:57 +0200

    content type for media

--- web/media_handler.go
+++ web/media_handler.go
@@ -23,6 +23,7 @@ func (h *MediaHandler) Handle(c *fiber.Ctx) error {
 		return err
 	}
 	binary, err := h.binaryService.FindById(id)
+	c.Set("Content-Type", binary.Mime())
 	if err != nil {
 		return err
 	}
