fc62d74ac0e0ad4209896c013ab6bcee4a8cd5da
Niko Abeler <niko@rerere.org>
2024-06-01 13:29:59 +0200

    list tags and adjusted hashtag extraction

--- entry_types/utils.go
+++ entry_types/utils.go
@@ -1,18 +1,39 @@
 package entrytypes
 
 import (
-	"regexp"
-	"strings"
+	"unicode"
 )
 
+func endOfHashtag(r rune) bool {
+	return !unicode.IsLetter(r) &&
+		!unicode.IsDigit(r) &&
+		r != '_' && r != '-' && r != '/'
+}
+
 func extractTags(content string) []string {
-	r := regexp.MustCompile("#[a-zA-Z0-9_]+")
-	matches := r.FindAllString(string(content), -1)
-	tags := make([]string, 0)
-	for _, hashtag := range matches {
-		tag, _ := strings.CutPrefix(hashtag, "#")
-		tags = append(tags, tag)
+	tagMap := make(map[string]bool)
+	start := -1
+	for i, c := range content {
+		if start != -1 {
+			if endOfHashtag(c) {
+				if i != start {
+					tagMap[content[start:i]] = true
+				}
+				start = -1
+			}
+		} else {
+			if c == rune('#') && (i == 0 || content[i-1] == ' ' || content[i-1] == '\n') {
+				start = i + 1
+				continue
+			}
+		}
+	}
+	if start != -1 && len(content)+1 > start {
+		tagMap[content[start:]] = true
+	}
+	tags := make([]string, 0, len(tagMap))
+	for t := range tagMap {
+		tags = append(tags, t)
 	}
 	return tags
-
 }
